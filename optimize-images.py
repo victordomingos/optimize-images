8f5a4075809c22e825a29296907cc24bdd8d5698
victordomingos <editor.arcosonline@gmail.com>
2018-06-19 13:03:22 +0100

    updated docstring

--- optimize-images.py
+++ optimize-images.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 """
-A little CLI utility written in Python to apply some compression to images
-using PIL
+A little command-line interface (CLI) utility written in pure Python to help
+you reduce the file size of images.
+
+You must explicitly pass it a path to the source image file or to the
+directory containing the image files to be processed. By default, it will go
+through all of its subdirectories and try to optimize the images found. You
+may however choose to process the specified directory only, without recursion.
+
+Please note that the operation is done DESTRUCTIVELY, by replacing the
+original files with the processed ones. You definitely should duplicate the
+source file or folder before using this utility, in order to be able to
+recover any eventual damaged files or any resulting images that don't have the
+desired quality.
+
+This application is intended to be pure Python, with no special dependencies
+besides Pillow, therefore ensuring compatibility with a wide range of systems,
+including iPhones and iPads running Pythonista 3. If you don't have the need
+for such a strict dependency management, you will certainly be better served
+by any several other image optimization utilities that are based on some well
+known external binaries.
 
 © 2018 Victor Domingos (MIT License)
 """
