d1043a48dc2a0710ff7d5b3c45a4dbc2c596e5b0
victordomingos <editor.arcosonline@gmail.com>
2018-10-02 19:46:02 +0100

    exploring on dynamic optimization options

--- optimize_images/img_dynamic_quality.py
+++ optimize_images/img_dynamic_quality.py
@@ -2,6 +2,8 @@
 Adapted from:
 https://engineeringblog.yelp.com/2017/06/making-photos-smaller.html
 """
+import numpy as np
+
 from io import BytesIO
 from math import log
 from optimize_images.constants import DEFAULT_QUALITY
@@ -77,6 +79,15 @@ def compare_images_pil(img1, img2):
     return diff_ratio * 100
 
 
+def compare_images_np(img1, img2):
+    # Load images, convert to RGB, then to numpy arrays and ravel into long, flat things
+    a = np.array(img1.convert('RGB')).ravel()
+    b = np.array(img2.convert('RGB')).ravel()
+
+    # Calculate the sum of the absolute differences divided by number of elements
+    MAE = np.sum(np.abs(np.subtract(a,b,dtype=np.float))) / a.shape[0]
+    return MAE * 100
+
 
 def get_diff_at_quality(photo, quality):
     """Return a difference score for this JPEG image saved at the specified quality
@@ -120,6 +131,31 @@ def get_diff_at_quality_pil(photo, quality):
         return -1 + diff_score / 100
     else:
         return 1 - diff_score / 100
+
+
+
+
+def get_diff_at_quality_np(photo, quality):
+    """Return a difference score for this JPEG image saved at the specified quality
+
+    A SSIM score would be much better, but currently there is no pure Python
+    implementation available.
+    """
+    diff_photo = BytesIO()
+    # optimize is omitted here as it doesn't affect
+    # quality but requires additional memory and cpu
+    photo.save(diff_photo, format="JPEG", quality=quality, progressive=True)
+    diff_photo.seek(0)
+    diff_score = compare_images_np(photo, Image.open(diff_photo))
+
+    #print("================> DIFF1 == DIFF2? ", diff_score==diff_score2)
+
+    if diff_score < 0:
+        return -1 + diff_score / 100
+    else:
+        return 1 - diff_score / 100
+
+
 
 
 def _diff_iteration_count(lo, hi):
@@ -175,6 +211,7 @@ def jpeg_dynamic_quality(original_photo, use_dynamic_quality=True):
         default_diff = get_diff_at_quality(photo, hi)
         return hi, default_diff
 
+
 def jpeg_dynamic_quality_pil(original_photo, use_dynamic_quality=True):
     """Return an integer representing the quality that this JPEG image should be
     saved at to attain the quality threshold specified for this photo class.
@@ -218,4 +255,51 @@ def jpeg_dynamic_quality_pil(original_photo, use_dynamic_quality=True):
         return selected_quality, selected_diff
     else:
         default_diff = get_diff_at_quality_pil(photo, hi)
+        return hi, default_diff
+
+
+
+def jpeg_dynamic_quality_np(original_photo, use_dynamic_quality=True):
+    """Return an integer representing the quality that this JPEG image should be
+    saved at to attain the quality threshold specified for this photo class.
+
+    Args:
+        original_photo - a prepared PIL JPEG image (only JPEG is supported)
+    """
+    diff_goal = 0.992
+    hi = DEFAULT_QUALITY
+    lo = hi - 5
+
+    # working on a smaller size image doesn't give worse results but is faster
+    # changing this value requires updating the calculated thresholds
+    photo = original_photo.resize((400, 400))
+
+    if not use_dynamic_quality:
+        default_diff = get_diff_at_quality_np(photo, hi)
+        return hi, default_diff
+
+    # 95 is the highest useful value for JPEG. Higher values cause different behavior
+    # Used to establish the image's intrinsic ssim without encoder artifacts
+    normalized_diff = get_diff_at_quality_np(photo, 95)
+
+    selected_quality = selected_diff = None
+
+    # loop bisection. ssim/diff function increases monotonically so this will converge
+    for i in range(_diff_iteration_count(lo, hi)):
+        curr_quality = (lo + hi) // 2
+        curr_diff = get_diff_at_quality_np(photo, curr_quality)
+        diff_ratio = curr_diff / normalized_diff
+
+        if diff_ratio >= diff_goal:
+            # continue to check whether a lower quality level also exceeds the goal
+            selected_quality = curr_quality
+            selected_diff = curr_diff
+            hi = curr_quality
+        else:
+            lo = curr_quality
+
+    if selected_quality:
+        return selected_quality, selected_diff
+    else:
+        default_diff = get_diff_at_quality_np(photo, hi)
         return hi, default_diff
--- optimize_images/img_optimize_jpg.py
+++ optimize_images/img_optimize_jpg.py
@@ -13,7 +13,7 @@ except ImportError:
 from optimize_images.data_structures import Task, TaskResult
 from optimize_images.img_aux_processing import make_grayscale
 from optimize_images.img_aux_processing import downsize_img
-from optimize_images.img_dynamic_quality import jpeg_dynamic_quality, jpeg_dynamic_quality_pil
+from optimize_images.img_dynamic_quality import jpeg_dynamic_quality, jpeg_dynamic_quality_pil, jpeg_dynamic_quality_np
 
 
 def optimize_jpg(t: Task) -> TaskResult:
@@ -61,7 +61,8 @@ def optimize_jpg(t: Task) -> TaskResult:
         quality = t.quality
     else:
         #quality, jpgdiff = jpeg_dynamic_quality(img)
-        quality, jpgdiff = jpeg_dynamic_quality_pil(img)
+        #quality, jpgdiff = jpeg_dynamic_quality_pil(img)
+        quality, jpgdiff = jpeg_dynamic_quality_np(img)
 
         #q_is_the_same = "Yo!" if quality == quality_pil else f"xxxxxxx Nope! {quality_pil/quality:.3f}%"
         #diff_is_the_same = "Yo!" if jpgdiff == jpgdiff_pil else f"xxxxxxx Nope! {jpgdiff_pil/jpgdiff:.3f}%"
