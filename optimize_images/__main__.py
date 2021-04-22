9c9bd3cabcbf1e8780e197bb6c3308be59d4d6e7
Tharindu N <40334396+truethari@users.noreply.github.com>
2021-04-22 09:59:06 +0530

    Fixed the error when installing the package

--- optimize_images/__main__.py
+++ optimize_images/__main__.py
@@ -64,10 +64,10 @@ def main():
     args = get_args()
     optimizer(*args)
 
-def optimizer(src_path, watch_dir=False, recursive=True, quality=80, remove_transparency=False,
-     reduce_colors=False, max_colors=256, max_w=0, max_h=0, keep_exif=False, convert_all=False, 
-     conv_big=False, force_del=False, bg_color=(255, 255, 255), grayscale=False,
-     ignore_size_comparison=False, fast_mode=False, jobs=0):
+def optimizer(src_path, watch_dir, recursive, quality, remove_transparency,
+     reduce_colors, max_colors, max_w, max_h, keep_exif, convert_all, 
+     conv_big, force_del, bg_color, grayscale,
+     ignore_size_comparison, fast_mode, jobs):
     appstart = timer()
     line_width, our_pool_executor, workers = adjust_for_platform()
     if jobs != 0:
---
+++ optimize_images/api.py
@@ -0,0 +1,9 @@
+def optimizer(src_path, watch_dir=False, recursive=True, quality=80, remove_transparency=False,
+     reduce_colors=False, max_colors=256, max_w=0, max_h=0, keep_exif=False, convert_all=False,
+     conv_big=False, force_del=False, bg_color=(255, 255, 255), grayscale=False,
+     ignore_size_comparison=False, fast_mode=False, jobs=0):
+    from optimize_images.__main__ import optimizer as app
+    app(src_path, watch_dir, recursive, quality, remove_transparency,
+     reduce_colors, max_colors, max_w, max_h, keep_exif, convert_all,
+     conv_big, force_del, bg_color, grayscale,
+     ignore_size_comparison, fast_mode, jobs)
